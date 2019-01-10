c65a72361b69ce298b96603a0ecfce1b59a04502
Jake Bowkett <jake.bowkett01@gmail.com>
2019-01-11 03:46:02 +1100

    Stopped wrapping http.ResponseWriter. Added variable support for patterns.

--- router/router.go
+++ router/router.go
@@ -1,45 +1,37 @@
 package router
 
 import (
+	"fmt"
 	"net/http"
 	"strings"
 	"time"
 )
 
-type Handler func(reqId string, w http.ResponseWriter, r *http.Request)
-type Deferred func(reqId string, status, duration int, r *http.Request)
+type Handler = func(reqId string, w http.ResponseWriter, r *http.Request, vars Vars)
+type Deferred = func(reqId string, duration int, r *http.Request)
 
-type resWriterWrapper struct {
-	status int
-	rw     http.ResponseWriter
+type segment struct {
+	raw     string
+	varName string
+	matches []string
 }
 
-func (rw *resWriterWrapper) Header() http.Header {
-	return rw.rw.Header()
-}
-
-func (rw *resWriterWrapper) Write(b []byte) (int, error) {
-	return rw.rw.Write(b)
-}
-
-func (rw *resWriterWrapper) WriteHeader(code int) {
-	rw.status = code
-	rw.rw.WriteHeader(code)
-}
+type Vars = map[string]string
 
 type Router struct {
-	methods     []string
-	routes      [][]string
-	handlers    []Handler
-	NotFound    Handler
-	IdGenerator func() (id string)
-	Deferred    func(reqId string, status, duration int, r *http.Request)
+	methods       []string
+	patterns      [][]segment
+	handlers      []Handler
+	NotFound      Handler
+	Errors        []error
+	BeforeHandler func(reqId string)
+	IdGenerator   func() (id string)
+	Deferred      Deferred
 }
 
 func (rt *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	start := time.Now()
-	w = &resWriterWrapper{rw: w}
 
 	// Request ID is an empty string if there's no generator.
 	var reqId string
@@ -47,83 +39,167 @@ func (rt *Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		reqId = rt.IdGenerator()
 	}
 
+	if rt.BeforeHandler != nil {
+		rt.BeforeHandler(reqId)
+	}
+
 	if rt.Deferred != nil {
-		defer func() {
-			status := -1
-			wrapper, ok := w.(*resWriterWrapper)
-			if ok {
-				status = wrapper.status
-				if status == 0 {
-					status = 200
-				}
-			}
-			duration := int(time.Since(start).Nanoseconds())
-			rt.Deferred(reqId, status, duration, r)
-		}()
+		defer rt.Deferred(reqId, int(time.Since(start).Nanoseconds()), r)
 	}
 
 	reqPath := explodePath(r.URL.Path)
 
-	for i := range rt.routes {
+	for i := range rt.patterns {
 		if rt.methods[i] != r.Method {
 			continue
 		}
-		if !pathsMatch(rt.routes[i], reqPath) {
+		vars, ok := pathsMatch(rt.patterns[i], reqPath)
+		if !ok {
 			continue
 		}
-		rt.handlers[i](reqId, w, r)
+		rt.handlers[i](reqId, w, r, vars)
 		return
 	}
 
 	if rt.NotFound != nil {
-		rt.NotFound(reqId, w, r)
+		rt.NotFound(reqId, w, r, make(Vars))
 		return
 	}
 
 	w.WriteHeader(http.StatusNotFound)
 }
 
-func (rt *Router) Get(route string, handler func(string, http.ResponseWriter, *http.Request)) {
-	rt.add("GET", route, handler)
+func (rt *Router) Get(pattern string, handler Handler) {
+	rt.add("GET", pattern, handler)
 }
 
-func (rt *Router) Post(route string, handler func(string, http.ResponseWriter, *http.Request)) {
-	rt.add("POST", route, handler)
+func (rt *Router) Post(pattern string, handler Handler) {
+	rt.add("POST", pattern, handler)
 }
 
-func (rt *Router) add(method, route string, handler Handler) {
+func (rt *Router) add(method, pattern string, handler Handler) {
 	rt.methods = append(rt.methods, method)
-	rt.routes = append(rt.routes, explodePath(route))
+	rt.patterns = append(rt.patterns, rt.expandPattern(pattern))
 	rt.handlers = append(rt.handlers, handler)
 }
 
-func pathsMatch(routerPath, requestPath []string) bool {
-
-	if len(routerPath) != len(requestPath) {
-		return false
+func illegalChar(pattern, kind, chars string) error {
+	var s string
+	cc := strings.Split(chars, "")
+	for i, c := range cc {
+		if i == len(cc)-1 {
+			s += fmt.Sprintf(" or %q", c)
+			break
+		}
+		s += fmt.Sprintf("%q,", c)
 	}
+	return fmt.Errorf("pattern segment %s cannot contain %s\npattern: %q", kind, s, pattern)
+}
 
-	for i := range routerPath {
+func (rt *Router) expandPattern(pattern string) []segment {
 
-		rp := routerPath[i]
+	var segments []segment
+	subPatterns := explodePath(pattern)
 
-		if rp == "*" {
-			continue
+	for _, sp := range subPatterns {
+
+		var literal *string
+		var varName string
+		var matches []string
+		var listStart int
+		var errs []error
+		illegal := ":[]"
+
+		switch {
+
+		// Literal segment.
+		case sp[0] != ':' && sp[0] != '[':
+			if idx := strings.IndexAny(sp, illegal); idx != -1 {
+				errs = append(errs, illegalChar(pattern, "literal", illegal))
+			}
+			literal = &sp
+
+		// Segement with variable.
+		case sp[0] == ':':
+			listStart = strings.IndexRune(sp, '[')
+			if listStart == -1 {
+				varName = sp[1:]
+				break
+			}
+			varName = sp[1:listStart]
+			fallthrough
+
+		// Segment containing whitelist.
+		case sp[0] == '[':
+			if sp[len(sp)-1] != ']' {
+				errs = append(errs, fmt.Errorf(
+					`pattern segment contains "[" but doesn't end with "]"`+"\n"+
+						"pattern: %q", pattern))
+			}
+			matches = strings.Split(sp[listStart+1:len(sp)-1], ",")
 		}
 
-		if strings.HasPrefix(rp, "[") && strings.HasSuffix(rp, "]") {
-			rp := rp[1 : len(rp)-1]
-			if in(strings.Split(rp, "|"), requestPath[i]) {
-				continue
+		if idx := strings.IndexAny(varName, illegal); idx != -1 {
+			errs = append(errs, illegalChar(pattern, "variable", illegal))
+		}
+		for i := range matches {
+			matches[i] = strings.TrimSpace(matches[i])
+			if idx := strings.IndexAny(matches[i], illegal); idx != -1 {
+				errs = append(errs, illegalChar(pattern, "whitelist", illegal))
 			}
 		}
 
-		if rp != requestPath[i] {
-			return false
+		if literal != nil {
+			matches = []string{*literal}
+		}
+
+		if len(errs) > 0 {
+			rt.Errors = append(rt.Errors, errs...)
+			continue
+		}
+
+		segments = append(segments, segment{
+			raw:     sp,
+			varName: varName,
+			matches: matches,
+		})
+	}
+
+	return segments
+}
+
+func pathsMatch(pattern []segment, reqPath []string) (vars Vars, ok bool) {
+
+	vars = make(Vars)
+
+	if len(pattern) != len(reqPath) {
+		return nil, false
+	}
+
+	for i, seg := range pattern {
+
+		// wildcard segment
+		if seg.matches == nil {
+			if seg.varName != "" {
+				vars[seg.varName] = reqPath[i]
+			}
+			continue
+		}
+
+		if !in(seg.matches, reqPath[i]) {
+			return nil, false
+		}
+
+		if seg.varName != "" {
+			vars[seg.varName] = reqPath[i]
 		}
 	}
 
-	return true
+	if len(vars) == 0 {
+		vars = nil
+	}
+
+	return vars, true
 }
 
 func in(ss []string, s string) bool {
@@ -135,6 +211,6 @@ func in(ss []string, s string) bool {
 	return false
 }
 
-func explodePath(route string) []string {
-	return strings.Split(strings.Trim(route, "/"), "/")
+func explodePath(path string) []string {
+	return strings.Split(strings.Trim(path, "/"), "/")
 }
